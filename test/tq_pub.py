65cc7143ae174177874b9175c6f997c56b271337
kim-hc <joel9537@gmail.com>
2021-12-30 10:10:27 +0900

    working version

--- test/tq_pub.py
+++ test/tq_pub.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import JointState
 
 if __name__ == "__main__":
     rospy.init_node('tq_publisher')
-    desired_pub = rospy.Publisher('dyros_mobile/desired_joint', JointState, queue_size=5)
+    desired_pub = rospy.Publisher('/dyros_mobile/desired_joint', JointState, queue_size=5)
 
     start_time = rospy.Time.now()
     js_ = JointState(
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     r = rospy.Rate(HZ)
     play_time = 5.0
 
-    value_steer = 80
-    value_roll = 50
+    value_steer = 800
+    value_roll  = 0
 
     for i in range(4):
         s = (i+1)*2  ## steering node
@@ -34,5 +34,6 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         js_.header.stamp = rospy.Time.now()
         desired_pub.publish(js_)
+        print(js_)
         if (rospy.Time.now() - start_time).to_sec() > play_time: break
         r.sleep()
